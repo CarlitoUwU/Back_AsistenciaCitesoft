d2cba53f7303272b087c04a13191a3e1c120ae81
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-05 19:13:38 -0500

    agrega conexxion a db

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg')
+
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+})
+
+module.exports = pool
