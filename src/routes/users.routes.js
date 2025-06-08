b73926783fe50e5627e6560f475af1d4769d3fc1
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-08 16:15:49 -0500

    agrega autenticación a las rutas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const router = Router()
 const usersController = require('../controllers/users.controller')
+const verifyToken = require('../middleware/authMiddleware')
 
-router.get('/', usersController.getAll)
-router.get('/:id', usersController.getById)
-router.post('/', usersController.create)
-router.put('/:id', usersController.update)
-router.delete('/:id', usersController.remove)
+router.get('/', verifyToken(['ADMIN']), usersController.getAll)
+router.get('/:id', verifyToken(['ADMIN']), usersController.getById)
+router.post('/', verifyToken(['ADMIN']), usersController.create)
+router.put('/:id', verifyToken(['ADMIN']), usersController.update)
+router.delete('/:id', verifyToken(['ADMIN']), usersController.remove)
 
 module.exports = router
