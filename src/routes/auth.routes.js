00481ea5646addeeca35bec3964424e72f43db21
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-13 10:05:27 -0500

    agrega documentacion para endpoints de auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,37 @@ const passport = require('passport')
 const router = express.Router()
 const { googleRedirect } = require('../controllers/auth.controller')
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Endpoints para autenticación
+ */
+
+/**
+ * @swagger
+ * /auth/google:
+ *   get:
+ *     summary: Iniciar autenticación con Google
+ *     tags: [Auth]
+ *     responses:
+ *       302:
+ *         description: Redirige a la página de autenticación de Google
+ */
+
+/**
+ * @swagger
+ * /auth/google/redirect:
+ *   get:
+ *     summary: Redirección después de autenticación con Google
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description: Autenticación exitosa y redirige al cliente
+ *       401:
+ *         description: Error de autenticación
+ */
+
 // Inicia el flujo de autenticación
 router.get('/google',
   passport.authenticate('google', { scope: ['profile', 'email'] })
