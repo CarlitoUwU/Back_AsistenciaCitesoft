23cd16acd12a8696e6dc67b9d30e5657faa8752e
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-13 00:00:50 -0500

    agrega constantes

---
+++ src/constantes/weekday.js
@@ -0,0 +1,16 @@
+const WEEKDAY = {
+  MONDAY: 'monday',
+  TUESDAY: 'tuesday',
+  WEDNESDAY: 'wednesday',
+  THURSDAY: 'thursday',
+  FRIDAY: 'friday',
+  SATURDAY: 'saturday',
+  SUNDAY: 'sunday'
+}
+
+const WEEKDAY_LIST = Object.values(WEEKDAY)
+
+module.exports = {
+  WEEKDAY,
+  WEEKDAY_LIST
+}
